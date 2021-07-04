5eb007b147f3c4dd411f0d3bd3274cab694c34ae
viniciusog <vinijipa23@gmail.com>
2021-07-03 21:16:58 -0300

    switch and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import './App.css';
+
 import Layout from './components/Layout';
-import {Switch} from "react-router-dom"
+import {Redirect, Route, Switch} from "react-router-dom"
+import Home from "./pages/Home"
+import NewProduct from './pages/NewProduct';
+import Products from "./pages/Products"
+import ProductDetail from "./pages/ProductDetails"
 
 function App() {
   return (
    <Layout>
      <Switch>
-    
+        <Route path="/" exact>
+            <Home/>
+        </Route>
+
+        <Route path="/new-product" exact>
+            <NewProduct />
+        </Route>
+
+        {/* Para mostrar /products e /products/:productId juntos, é preciso criar a roa /products/:productId
+        dentro da página Products. No nosso caso, não queremos desta forma. */}
+        <Route path="/products" exact>
+          <Products/>
+        </Route>
+
+        <Route path="/products/:productId" exact>
+          <ProductDetail/>
+        </Route>
+
+        {/* Se tentar acessar uma rota que não existe, então manda para a home */}
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
      </Switch>
    </Layout>
   );
---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,8 @@
+const ProductDetail: React.FC = () => {
+
+    return (
+        <h1>Product detail</h1>
+    )
+}
+
+export default ProductDetail
