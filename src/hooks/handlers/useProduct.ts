782dd92278cb9c160d52f3d770fc17cd9e839cb8
viniciusog <vinijipa23@gmail.com>
2021-07-06 20:19:18 -0300

    load form values and edit product

--- src/hooks/handlers/useProduct.ts
+++ src/hooks/handlers/useProduct.ts
@@ -11,6 +11,8 @@ interface UseProduct {
     onFinish(): void,
     onEdit(toEditProduct: Product): void,
     onDelete(id: string): void,
+    onGetProductById(id: string): Product | null,
+    onLoadForm(id: string): void,
     products: Product[]
 }
 
@@ -31,6 +33,7 @@ const useProduct = (): UseProduct => {
             id: Math.random().toString(), 
             name: product.name, 
             description: product.description, 
+            //passando de moment para string
             expirationdate: moment(product.expirationdate).format("YYYY-MM-DD")
         })
 
@@ -41,8 +44,20 @@ const useProduct = (): UseProduct => {
     }
 
     //O antd passará o produto que deverá ser atualizado, automaticamente
+    //toEditProduct possui os todos os dados, porém o 'expirationdate' é um objeto moment. Por isso, precisamos
+    //passar esse tipo moment para string
     const onEdit = (toEditProduct: Product) => {
-        productContext.onEditProduct(toEditProduct.id, toEditProduct)
+        productContext.onEditProduct(toEditProduct.id, 
+            {
+                //Pega todos os dados atualizados do produto
+                ...toEditProduct, 
+                //porém, passaremos o 'expirationdate' de moment para string
+                expirationdate: moment(toEditProduct.expirationdate).format("YYYY-MM-DD")
+            }
+        )
+
+        form.resetFields()
+
         return message.success("Produto editado com sucesso!")
     }
 
@@ -51,11 +66,36 @@ const useProduct = (): UseProduct => {
         return message.success("Produto deletado com sucesso!")
     }
 
+    //É usado na página de editar produto (details page)
+    //Apenas usamos isso para verificar se temos um produto com id igual ao recebido pela url
+    const onGetProductById = (id: string): Product | null => {
+        return productContext.getProductById(id)
+    }
+
+    //Carrega os dados de produto no formulário quando acessamos a página de editar produto
+    const onLoadForm = (id: string): void => {
+        const product = productContext.getProductById(id)
+        
+        if (product !== null || product !== undefined) {
+            //coloquei o '!' pois tenho certeza que nesse momento o product não será null ou undefined
+            //coloquei object pois o tipo Product não aceita expirationdate sendo 'moment', apenas string.
+            const productWithCorrectExpirationDate: object = {
+                ...product, 
+                //estamos passando de string para moment. O DatePicker aceita apenas moment.
+                expirationdate: moment(product?.expirationdate, "YYYY-MM-DD")
+            }
+
+            form.setFieldsValue(productWithCorrectExpirationDate!)
+        }   
+    }
+
     return {
         form,
         onFinish,
         onEdit,
         onDelete,
+        onGetProductById,
+        onLoadForm,
         products: productContext.products
     }
 }
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,10 +1,9 @@
-import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react"
+import { useContext} from "react"
 import { useRouteMatch } from "react-router-dom"
-import { useParams, useHistory } from "react-router-dom"
-import { ProductContext } from "../store/products-context"
 import { PagesContext } from "../store/pages-context"
 import { Form, DatePicker, Button, Input, Alert } from "antd"
-import { Product } from "../models/product"
+import {useProduct} from "../hooks/handlers/useProduct"
+
 import moment from "moment"
 
 type ProductDetailParams = {
@@ -13,74 +12,23 @@ type ProductDetailParams = {
 
 const ProductDetail: React.FC = () => {
 
-    const history = useHistory()
-
-    //Tem que ser productId, pois eu defini, na página Products, essa Route => /products/:productId
-    const route = useRouteMatch<ProductDetailParams>()
-    const productId = route.params.productId
+    const {form, onEdit, onGetProductById, onLoadForm} = useProduct()
 
     //Contexto das páginas
     const pagesContext = useContext(PagesContext)
     pagesContext.changeHeaderTitle("Product details")
 
-    //Contexto de produto
-    const productContext = useContext(ProductContext)
-    const product = productContext.getProductById(productId)
+    //Tem que ser productId, pois eu defini, na página Products, essa Route => /products/:productId
+    const route = useRouteMatch<ProductDetailParams>()
+    const productId = route.params.productId
 
-    //Estados
-    const [name, setName] = useState<string>("")
-    const [description, setDescription] = useState<string>("")
-    const [expirationDate, setExpirationDate] = useState<string>("2021-01-01")
-
-    useEffect(() => {
-        if (product !== null && product != undefined) {
-            setName(product.name)
-            setDescription(product.description)
-            setExpirationDate(product.expirationdate)
-        }
-    }, [product])
+    //Estamos inserindo valores no form, de acordo com o produto com id correspondente
+    onLoadForm(productId)
+    //Estamos pegando o produto de acordo com o id da url
+    const product = onGetProductById(productId)
 
 
-    const nameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value)
-    }
-
-    const descriptionChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setDescription(event.target.value)
-    }
-
-    const expirationDateChangeHandler = (date: moment.Moment | null, dateString: string) => {
-        setExpirationDate(dateString)
-    }
-
-    const formSubmitHandler = (event: FormEvent) => {
-        if (product) {
-            const enteredName = name
-            const enteredDescription = description
-            const enteredExpirationDate = expirationDate
-
-            if (enteredName.trim().length > 0
-                && enteredDescription.trim().length > 0
-                && enteredExpirationDate.trim().length > 0) {
-                productContext.onEditProduct(productId, {
-                    id: productId,
-                    name: enteredName,
-                    description: enteredDescription,
-                    expirationdate: enteredExpirationDate
-                })
-
-                alert("Product updated!")
-
-                setName("")
-                setDescription("")
-                setExpirationDate("2021-01-01")
-
-                history.replace("/products")
-            } else {
-                alert("Values must not be empty!")
-            }
-        }
-    }
+    console.log(product)
 
     if (product === null || product === undefined) {
         return <Alert message={`Product not found with id: ${productId}`} type="error"
@@ -95,21 +43,22 @@ const ProductDetail: React.FC = () => {
             labelAlign="left"
             labelCol={{ span: 6 }}
             layout="horizontal"
-            onFinish={formSubmitHandler}
+            form={form}
+            onFinish={onEdit}
 
             style={{ margin: "auto", marginTop: "20px", maxWidth: "min(600px, 90%)" }}
         >
-            <Form.Item label="Id">
+            <Form.Item label="Id" name="id">
                 <Input disabled value={productId}></Input>
             </Form.Item>
-            <Form.Item label="Name">
-                <Input onChange={nameChangeHandler} value={name}></Input>
+            <Form.Item label="Name" name="name">
+                <Input ></Input>
             </Form.Item>
-            <Form.Item label="Description">
-                <Input onChange={descriptionChangeHandler} value={description}></Input>
+            <Form.Item label="Description" name="description">
+                <Input></Input>
             </Form.Item>
-            <Form.Item label="Expiration date">
-                <DatePicker style={{ width: "100%" }} onChange={expirationDateChangeHandler} value={moment(expirationDate, "YYYY-MM-DD")}>
+            <Form.Item label="Expiration date" name="expirationdate">
+                <DatePicker style={{ width: "100%" }}>
 
                 </DatePicker>
             </Form.Item>
